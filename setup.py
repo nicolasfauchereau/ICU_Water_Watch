e590b04cb898e474c970cfe0b1e687c80855ccbe
Nicolas Fauchereau <Nicolas.Fauchereau@gmail.com>
2021-05-06 09:58:05 +1200

    added license and setup.py file, installation via python setup.py install not tested yet

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='ICU_Water_Watch',
+    version='0.1',
+    author = "Nicolas Fauchereau",
+    author_email = "Nicolas.Fauchereau@niwa.co.nz",
+    description = ("A set of functions for the processing of GPM-IMERG satellite rainfall and C3S MME forecasts"),
+    url = "https://github.com/nicolasfauchereau/ICU_Water_Watch",
+    license = "LICENSE.txt",
+    long_description = open('README.md').read(),
+    py_modules=['C3S','geo','GPM','plot','utils','verification'],
+)
