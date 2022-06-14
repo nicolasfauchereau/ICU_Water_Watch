1674643d70e9237942f46de4033f028a22bccd04
Nicolas Fauchereau <Nicolas.Fauchereau@gmail.com>
2022-06-15 10:48:05 +1200

    calc_accuracy_sco now takes a float  argument, expressed in percentage

--- ICU_Water_Watch/verification.py
+++ ICU_Water_Watch/verification.py
@@ -698,5 +698,41 @@ def true_negatives(dset, class_value=1, forecast_var='gcm', verification_var='ve
                           output_dtypes =[float])
     return tnegatives
 
+def calc_accuracy_sco(df, tolerance=None): 
+    
+    """
+    calculates the 'SCO' accuracy, which 'optionally' allows for a 5% tolerance
+    
+    Arguments
+    ---------
+    
+    df : the pandas DataFrame containing 4 columns: 
+    
+        0: the observed category from 1 to n categories
+        1: the probability in percentage (0 to 100) for the first category
+        2: the probability in percentage (0 to 100) for the first category
+        3: the probability in percentage (0 to 100) for the first category
+        ...  
+        
+    tolerance : Boolean, whether or not to turn the tolerance on 
+    
+    Return
+    ------
+    
+    acc : the accuracy from 0 to 1 
+    
+    
+    """
 
+    import numpy as np 
+    import pandas as pd
 
+    acc = []
+    for i in range(len(df)):
+        if tolerance is not None: 
+            r = (int(df.iloc[i,0]) == df.iloc[i,1:].idxmax()) or ((df.iloc[i, df.iloc[i,1:].idxmax()] - df.iloc[i,int(df.iloc[i,0])]) <= tolerance)
+        else: 
+            r = (int(df.iloc[i,0]) == df.iloc[i,1:].idxmax())
+        acc.append(r)
+    acc = np.array(acc)
+    return acc.sum() / len(acc)
